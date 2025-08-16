3c5236aafbe02d7f6bde463ae8d97f6ff34feba6
bepinyd <bepinyd@gmail.com>
2025-08-16 20:50:32 +0530

    New Update

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,9 +3,13 @@ import { checkAuth, login, logout, signup, updateProfile } from "../controllers/
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
 router.put("/update-profile", protectRoute, updateProfile);
+
 router.get("/check", protectRoute, checkAuth);
+
 export default router;
